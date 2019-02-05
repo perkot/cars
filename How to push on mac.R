463da182f5b175da62e952e803e0b8e329b8c080
Tom Perkins <perkot@gmail.com>
2019-02-05 12:47:05 +1100

    added instructions on pushing via mac

---
+++ How to push on mac.R
@@ -0,0 +1,30 @@
+#https://thesamuelsoncondition.com/2016/01/29/r-studio-git-github-and-osx/
+
+#$git clone https://github.com/perkot/cars
+      #Cloning into 'cars'...
+      #remote: Counting objects: 10, done.
+      #remote: Total 10 (delta 0), reused 0 (delta 0), pack-reused 10
+      #Unpacking objects: 100% (10/10), done.
+      #Checking connectivity... done
+
+#$cd cars
+
+#$git push
+      #warning: push.default is unset; its implicit value is changing in
+      #Git 2.0 from 'matching' to 'simple'. To squelch this message
+      #and maintain the current behavior after the default changes, use:
+  
+  #git config --global push.default matching
+
+#To squelch this message and adopt the new behavior now, use:
+  
+  #git config --global push.default simple
+
+#See 'git help config' and search for 'push.default' for further information.
+#(the 'simple' mode was introduced in Git 1.7.11. Use the similar mode
+  #'current' instead of 'simple' if you sometimes use older versions of Git)
+
+#Username for 'https://github.com':
+#Password for 'https://perkot@github.com': 
+  
+  #Everything up-to-date
